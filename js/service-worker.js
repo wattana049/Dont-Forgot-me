49267cdbb98f21265b558c25f5d89d2fc07b98dc
wattana049 <pgodzgaming@gmail.com>
2024-12-10 19:05:37 +0700

    Delete js directory

--- js/service-worker.js
+++
@@ -1,8 +0,0 @@
-self.addEventListener('push', function (event) {
-    const data = event.data.json();
-    self.registration.showNotification(data.title, {
-      body: data.body,
-      icon: 'icon-url.png',
-    });
-  });
-  
